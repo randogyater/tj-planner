7787bd8232fadef5a99494c2495e8817b765e155
greenturtle1134 <daniel.y.fu@gmail.com>
2020-04-24 13:00:19 -0400

    Changed JSON structure to use ID as key
    Also renamed the file to match

--- prereq_normalizer.py
+++ prereq_normalizer.py
@@ -3,7 +3,7 @@ import os
 import pickle
 
 SOURCE = "courses_cleaned.json"
-DESTINATION = "courses_with_depend.json"
+DESTINATION = "courses_by_id.json"
 ALIAS_CACHE = "alias.pkl"
 
 
@@ -18,6 +18,9 @@ def main():
             cache = pickle.load(cache_file)
             if input("Found existing alias dict: %s\nUse this? (y/n) " % (cache,)) == "y":
                 alias = cache
+    id_map = dict()
+    for course in data:
+        id_map[course["short_name"]] = course["id"]
     for course in data:
         if len(course["prerequisites"]) == 0:
             del course["prerequisites"]
@@ -26,22 +29,27 @@ def main():
             for prereq_set in course["prerequisites"]:
                 new_set = list()
                 for prereq in prereq_set:
+                    new_prereq = None
                     if prereq in names:
-                        new_set.append(prereq)
+                        new_prereq = prereq
                     elif prereq in alias:
-                        new_set.append(alias[prereq])
+                        new_prereq = alias[prereq]
                     else:
                         new_name = input("Course \"%s\" not found; enter alias: " % (prereq,))
                         while new_name != "" and new_name not in names:
                             new_name = input("Course \"%s\" not found; try again? " % (new_name,))
                         if new_name != "":
                             alias[prereq] = new_name
-                            new_set.append(new_name)
-                        else:
-                            new_set.append(prereq + "(?)")
+                            new_prereq = new_name
+                    if new_prereq is not None:
+                        new_prereq = id_map[new_prereq]
+                        new_set.append(new_prereq)
+                        if new_prereq not in depends:
+                            depends[new_prereq] = list()
+                        depends[new_prereq].append(course["short_name"])
                 new_prereqs.append(new_set)
             course["prerequisites"] = new_prereqs
-        if course["weight"] > 0.6: # I'm not sure about precision so use >0.6 instead of >0.5
+        if course["weight"] > 0.6:  # I'm not sure about precision so let's use >0.6 instead of >0.5
             if course["full_name"].startswith("AP"):
                 course["ap"] = "ap"
             else:
@@ -50,17 +58,15 @@ def main():
             course["ap"] = "pre"
     with open(ALIAS_CACHE, 'wb') as cache_file:
         pickle.dump(alias, cache_file)
+    result = dict()
     for course in data:
-        for prereq_set in course["prerequisites"]:
-            for prereq in prereq_set:
-                if prereq not in depends:
-                    depends[prereq] = list()
-                depends[prereq].append(course["short_name"])
-    for course in data:
-        if course["short_name"] in depends:
-            course["dependents"] = depends[course["short_name"]]
+        if course["id"] in depends:
+            course["dependents"] = depends[course["id"]]
+        if course["id"] in result:
+            print("CONFLICTING COURSE ID: %s" % (course["id"],))
+        result[course["id"]] = course
     with open(DESTINATION, 'w', encoding="utf-8") as output_file:
-        output_file.write(json.dumps(data, indent=4))
+        output_file.write(json.dumps(result, indent=4))
 
 
 if __name__ == "__main__":
