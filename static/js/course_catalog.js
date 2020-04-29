8a6ff92483dd5b31fd970da94e4d0f7516049337
greenturtle1134 <daniel.y.fu@gmail.com>
2020-04-29 16:18:41 -0400

    Added icons for properties
    There's a problem with drag and drop and tooltip text, though

--- static/js/course_catalog.js
+++ static/js/course_catalog.js
@@ -2,6 +2,10 @@ $.getJSON("static/data/courses.json", function(data) {courses=data; showEntries(
 $.getJSON("static/data/labs.json", function(data) {labs=data});
 counter = 0;
 
+function getCourseNameString(id) {
+    return "("+id+") "+courses[id].full_name;
+}
+
 function createCourseDraggable(course_id) {
     course = courses[course_id];
     counter++;
@@ -44,6 +48,15 @@ function createCourseEntry(course_id) {
     }
     if(course.ap === "post") {
         $course.addClass("course--post-ap");
+    }
+    if(course.category === "Summer School") {
+        $course.append(" <i class=\"fas fa-sun\" title=\"This is a summer course replacing " + getCourseNameString(course.equivalent) + "\"></i>");
+    }
+    if(course.semester) {
+        $course.append(" <i class=\"fas fa-hourglass-start\" title=\"This is a semester course and will last half a year.\"></i>");
+    }
+    if(course.online) {
+        $course.append(" <i class=\"fas fa-desktop\" title=\"This course can be taken online.\"></i>");
     }
     return $course;
 }
