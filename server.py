0d1a34636cd4bf10cb3e0721a9e5d224311891b4
greenturtle1134 <daniel.y.fu@gmail.com>
2020-05-07 12:48:16 -0400

    Added config file and course list
    Also updated version of jquery

--- server.py
+++ server.py
@@ -1,8 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import yaml
+
+CONFIG_LOC = "config.yml"
 
 app = Flask(__name__)
 
 
+def get_config(field=None):
+    if "config" not in g:
+        with open(CONFIG_LOC, 'r') as file:
+            g.config = yaml.load(file)
+    if field:
+        return g.config[field]
+    else:
+        return g.config
+
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", categories=get_config("categories"))
