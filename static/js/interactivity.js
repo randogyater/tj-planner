4aa500ec39f0cfae9465c077c1925dbde506da04
greenturtle1134 <daniel.y.fu@gmail.com>
2020-05-08 15:34:48 -0400

    Implemented removing categories when empty

--- static/js/interactivity.js
+++ static/js/interactivity.js
@@ -180,19 +180,27 @@ function stringRequirements(x) {
 }
 
 function filter(condition) {
+    keep_category = new Set();
     $(".catalog__entry").each(function(i) {
         $this = $(this); // Sometimes it just looks surreal
         let course = courses[$this.attr("data-course-id")]
-        if(condition(course) && $this.is(":hidden")) {
-            $this.show("fast");
+        if(condition(course)) {
+            keep_category.add("catalog__"+toID(course.category));
+            if($this.is(":hidden")){
+                $this.show("fast");
+            }
         }
         if(!condition(course) && !$this.is(":hidden")) {
             $this.hide("fast");
         }
     });
     $(".catalog__category").each(function(i) {
-        $this = $(this);
-         let visible = this.children(".catalog__category__content").children(":hidden").length == 0;
-        console.log(visible);
+        let $this = $(this);
+        if(!keep_category.has($this.attr("id")) && $this.is(":visible")) {
+            $this.hide("fast");
+        }
+        if(keep_category.has($this.attr("id")) && $this.is(":hidden")) {
+            $this.show("fast");
+        }
     });
 }
