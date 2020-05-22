63ecb8dc5d91dd8266e659ac207bd1a9e98a2178
greenturtle1134 <daniel.y.fu@gmail.com>
2020-05-22 01:13:12 -0400

    Moved course property creation to HTML
    Currently have them as divs. Restoring the left and right alignment is annoying. I'll deal with that later.

--- static/js/course_catalog.js
+++ static/js/course_catalog.js
@@ -1,9 +1,5 @@
 $.getJSON("static/data/courses.json", function (data) {
     courses = data;
-    // for (course in courses) {
-    //     cat_id = toID(courses[course]["category"]);
-    //     $("#catalog__" + cat_id + ">.catalog__category__content").append(createCourseEntry(course));
-    // }
     $.getJSON("static/data/labs.json", function (data) {
         labs = data
         $.getJSON("static/data/default_schedule.json", function (defaults) {
@@ -64,56 +60,6 @@ function createCourseDraggable(course_id) {
     }
 
     $course.addClass("course--"+kebab(course.category))
-
-    return $course;
-}
-
-function createCourseEntry(course_id) {
-    course = courses[course_id];
-
-    var $course = $("<div>", {
-        id: "entry-" + course_id,
-        "class": "catalog__entry",
-        "draggable": "true",
-        "ondragstart": "dragStart(event)",
-        "data-course-id": course_id,
-    });
-
-    if (course.ap === "ap") {
-        $course.addClass("course--ap");
-    }
-
-    if (course.ap === "post") {
-        $course.addClass("course--post-ap");
-    }
-
-    $properties = $("<span class=\"course__properties\"></span>")
-    if (course.category === "Summer School") {
-        $properties.append(" <abbr title=\"This is a summer course replacing " + getCourseNameString(course.equivalent) + "\"><i class=\"fas fa-sun\"></i></abbr>");
-    }
-
-    if (course.skippable) {
-        $properties.append(" <abbr title=\"You can take a test to skip this course!\"><i class=\"fas fa-running\"></i></abbr>");
-    }
-
-    if (course.unique) {
-        $properties.append(" <abbr title=\"This course is unique to TJ and not offered elsewhere in FCPS.\"><i class=\"fas fa-asterisk\"></i></abbr>");
-    }
-
-    if (course.online) {
-        $properties.append(" <abbr title=\"This course can be taken online.\"><i class=\"fas fa-desktop\"></i></abbr>");
-    }
-
-    if (course.category === "Online") {
-        $properties.append(" <abbr title=\"This is an online-only course.\"><i class=\"fas fa-wifi\"></i></abbr>");
-    }
-
-    if (course.semester) {
-        $properties.append(" <span title=\"This is a semester course and will last half a year.\">½</abbr>");
-    }
-
-    $course.append($properties);
-    $course.append(course.full_name);
 
     return $course;
 }
